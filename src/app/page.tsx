72dc82023e7af227e797bf3b308ac070452bcfbc
vicuijpers <victor.cuijpers@odido.nl>
2025-08-11 21:19:14 +0200

    refactor: Replace HomePage with ComingSoonPage in main application entry
    - Updated the main application entry point to render the new Coming Soon page instead of the Home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import HomePage from "./pages/HomePage";
+import ComingSoonPage from "./pages/ComingSoonPage";
 
 export default function Home() {
-  return <HomePage />;
+  return <ComingSoonPage />;
 }
